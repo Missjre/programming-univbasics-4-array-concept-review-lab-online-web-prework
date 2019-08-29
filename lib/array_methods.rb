145d60a0ce73c6eaec4190e06cf6e893c98fa345
Jalysa Reason <jalysareason@gmail.com>
2019-08-29 22:30:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,11 +6,14 @@ def find_element_index(array, value_to_find)
    end
      nil
 end
-
+ 
 def find_max_value(array)
   max_value = 0
   array.lengh.times do |index| 
-    [2,3,4,5,2,1,4]
+    if array[index] > max_value
+      max_value = array[index]
+    end
+  end
   
 end
 
