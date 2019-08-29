0243223a94849b777bf8e1b7e0bf3478c4235135
Jalysa Reason <jalysareason@gmail.com>
2019-08-29 22:18:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,14 @@
 def find_element_index(array, value_to_find)
-  scale = [2,3,4,5]
-  scale_2 = [6,5,4,3]
-  hill = [1,2,3,4,5,4,3,2,1]
-  valley = [5,4,3,2,1,0,1,2,3,4,5]
-  zig_zag = [500,4,1000,5,250]
+  array.length.times do |index|
+    if array[index] == value_to_find 
+       return index 
+     end
 end
 
-def find_max_value(array)
-  find_max_value([1,2,1,3,4,3,5,4,3,2,1])
-end
+#def find_max_value(array)
+  
+#end
 
-def find_min_value(array)
-find_min_value([10,5,3,7,19,1,3,10])
-end
+#def find_min_value(array)
+
+#end
